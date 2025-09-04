4bf5d61873b8e4c3c993e4418de0c80279e777a7
EduardoMG12 <charles.eduardo.mello.guimaraes@hotmail.com>
2025-09-04 04:06:37 -0300

    chore: add lint format and config

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,40 +1,40 @@
-import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { GraphQLModule } from '@nestjs/graphql';
-import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
-import { join } from 'path';
-import { AppResolver } from './app.resolver';
-import { UserModule } from './user/user.module';
+import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import { GraphQLModule } from "@nestjs/graphql";
+import { ApolloDriver, ApolloDriverConfig } from "@nestjs/apollo";
+import { join } from "path";
+import { AppResolver } from "./app.resolver";
+import { UserModule } from "./user/user.module";
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-    }),
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      useFactory: async (configService: ConfigService) => ({
-        type: 'postgres',
-        host: configService.get<string>('DATABASE_HOST'),
-        port: configService.get<number>('DATABASE_PORT'),
-        username: configService.get<string>('DATABASE_USERNAME'),
-        password: configService.get<string>('DATABASE_PASSWORD'),
-        database: configService.get<string>('DATABASE_NAME'),
-        entities: [join(__dirname, '**', '*.entity{.ts,.js}')],
-        synchronize: true,
-      }),
-      inject: [ConfigService],
-    }),
-    GraphQLModule.forRoot<ApolloDriverConfig>({
-      driver: ApolloDriver,
-      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
-      // definitions:{
-      //    skipResolverArgs:true       
-      // }
-    }),
-    UserModule,
-  ],
-  providers: [AppResolver],
+	imports: [
+		ConfigModule.forRoot({
+			isGlobal: true,
+		}),
+		TypeOrmModule.forRootAsync({
+			imports: [ConfigModule],
+			useFactory: async (configService: ConfigService) => ({
+				type: "postgres",
+				host: configService.get<string>("DATABASE_HOST"),
+				port: configService.get<number>("DATABASE_PORT"),
+				username: configService.get<string>("DATABASE_USERNAME"),
+				password: configService.get<string>("DATABASE_PASSWORD"),
+				database: configService.get<string>("DATABASE_NAME"),
+				entities: [join(__dirname, "**", "*.entity{.ts,.js}")],
+				synchronize: true,
+			}),
+			inject: [ConfigService],
+		}),
+		GraphQLModule.forRoot<ApolloDriverConfig>({
+			driver: ApolloDriver,
+			autoSchemaFile: join(process.cwd(), "src/schema.gql"),
+			// definitions:{
+			//    skipResolverArgs:true
+			// }
+		}),
+		UserModule,
+	],
+	providers: [AppResolver],
 })
 export class AppModule {}
